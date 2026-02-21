626e4c31c69fa83f1c5ed2e9d201d49cf6762a51
So-diu <258138936+somath-edu@users.noreply.github.com>
2026-02-21 10:32:18 +0900

    5

--- quartz.config.ts
+++ quartz.config.ts
@@ -11,7 +11,7 @@ const config: QuartzConfig = {
     pageTitle: "생동주의",
     pageTitleSuffix: "",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
